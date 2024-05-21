c2ceb8606313d0378f6b3c0eeab705ca0dddd8b6
Shashank Kulkarni <44693969+KulkarniShashank@users.noreply.github.com>
2024-05-21 11:22:52 +0530

    Update on_premises_agent.sh

--- on_premises_agent.sh
+++ on_premises_agent.sh
@@ -18,15 +18,27 @@ download_script() {
 
 # URL of the script in the GitHub repository
 SCRIPT_URL="https://raw.githubusercontent.com/KulkarniShashank/on-premises-agent/master/on_premises_agent.sh"
+SCRIPT_NAME="on_premises_agent.sh"
+
+# Check if the script already exists
+if [ ! -f "$SCRIPT_NAME" ]; then
+    echo "Script not found, downloading..."
+    download_script "$SCRIPT_URL" "$SCRIPT_NAME"
+else
+    echo "Script already exists, skipping download."
+fi
+
+# Execute the script
+./"$SCRIPT_NAME"
+
+START_TIME=$(date +%s)
+DIR=".educreds"
 
 # Check if the script is being run directly or if the user wants to download it
 if [ "$1" == "--download" ]; then
     download_script "$SCRIPT_URL" "on_premises_agent.sh"
     exit 0
 fi
-
-START_TIME=$(date +%s)
-DIR=".educreds"
 
 # Check if the directory already exists
 if [ -d "$DIR" ]; then
@@ -146,27 +158,57 @@ echo "You selected: $TENANT"
 prompt_input "Enter CREDO_IMAGE: " CREDO_IMAGE
 prompt_input "Enter INBOUND_ENDPOINT: " INBOUND_ENDPOINT
 prompt_input "Enter ADMIN_PORT: " ADMIN_PORT
-prompt_input "Enter INBOUND_PORT: " INBOUND_PORT"
+prompt_input "Enter INBOUND_PORT: " INBOUND_PORT
 
-# Validate and format the INBOUND_ENDPOINT
-if [[ "$INBOUND_ENDPOINT" =~ ^https?:// ]]; then
-    AGENT_ENDPOINT="$INBOUND_ENDPOINT"
+on_premises_agent.sh --ORGANIZATION_ID "$ORGANIZATION_ID" --WALLET_NAME "$WALLET_NAME" --WALLET_PASSWORD "$WALLET_PASSWORD" --WEBHOOK_HOST "$WEBHOOK_HOST" --WALLET_STORAGE_HOST "$WALLET_STORAGE_HOST" --WALLET_STORAGE_PORT "$WALLET_STORAGE_PORT" --WALLET_STORAGE_USER "$WALLET_STORAGE_USER" --WALLET_STORAGE_PASSWORD "$WALLET_STORAGE_PASSWORD" --AGENT_NAME "$AGENT_NAME" --PROTOCOL "$PROTOCOL" --TENANT "$TENANT" --CREDO_IMAGE "$CREDO_IMAGE" --INBOUND_ENDPOINT "$INBOUND_ENDPOINT" --ADMIN_PORT "$ADMIN_PORT" --INBOUND_PORT "$INBOUND_PORT"
+
+# Run the command using user input
+# on_premises_agent.sh --ORGANIZATION_ID "$ORGANIZATION_ID" --WALLET_NAME "$WALLET_NAME" --WALLET_PASSWORD "$WALLET_PASSWORD" --WEBHOOK_HOST "$WEBHOOK_HOST" --WALLET_STORAGE_HOST "$WALLET_STORAGE_HOST" --WALLET_STORAGE_PORT "$WALLET_STORAGE_PORT" --WALLET_STORAGE_USER "$WALLET_STORAGE_USER" --WALLET_STORAGE_PASSWORD "$WALLET_STORAGE_PASSWORD" --AGENT_NAME "$AGENT_NAME" --PROTOCOL "$PROTOCOL" --TENANT "$TENANT" --CREDO_IMAGE "$CREDO_IMAGE" --INBOUND_ENDPOINT "$INBOUND_ENDPOINT" --ADMIN_PORT "$ADMIN_PORT" --INBOUND_PORT "$INBOUND_PORT"
+
+echo "admin port: $ADMIN_PORT"
+echo "inbound port: $INBOUND_PORT"
+
+echo "AGENT SPIN-UP STARTED"
+
+if [ -d "${PWD}/${DIR}/agent-config" ]; then
+    echo "agent-config directory exists."
 else
-    echo "Invalid input for INBOUND_ENDPOINT: $INBOUND_ENDPOINT"
-    exit 1
-fi
-
-echo "-----$AGENT_ENDPOINT----"
-CONFIG_FILE="${PWD}/${DIR}/agent-config/${ORGANIZATION_ID}_${AGENT_NAME}.json"
-
-# Ensure the agent-config directory exists
-if [ ! -d "${PWD}/${DIR}/agent-config" ]; then
-    echo "Error: agent-config directory does not exist."
-    mkdir -p ${PWD}/${DIR}/agent-config
+    echo "Error: agent-config directory does not exists."
+    mkdir ${PWD}/${DIR}/agent-config
 fi
 
 # Set ownership of the .educreds directory
 sudo chown $USER: "$DIR"
+
+# Define a regular expression pattern for IP address
+IP_REGEX="^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$"
+
+# Check if the input is a domain
+if echo "$INBOUND_ENDPOINT" | grep -qP "^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"; then
+    echo "INBOUND_ENDPOINT is a domain: $INBOUND_ENDPOINT"
+    AGENT_ENDPOINT=$INBOUND_ENDPOINT
+else
+    # Check if the input is an IP address
+    if [[ $INBOUND_ENDPOINT =~ $IP_REGEX ]]; then
+        echo "INBOUND_ENDPOINT is an IP address: $INBOUND_ENDPOINT"
+        AGENT_ENDPOINT="${PROTOCOL}://${INBOUND_ENDPOINT}:${INBOUND_PORT}"
+    else
+        echo "Invalid input for INBOUND_ENDPOINT: $INBOUND_ENDPOINT"
+    fi
+fi
+
+# Set permissions to restrict access to the agent-config directory
+# chmod 700 "${PWD}/${DIR}/agent-config"
+# echo "Permissions set to 700 for ${PWD}/${DIR}/agent-config"
+
+echo "-----$AGENT_ENDPOINT----"
+CONFIG_FILE="${PWD}/${DIR}/agent-config/${ORGANIZATION_ID}_${AGENT_NAME}.json"
+
+# Check if the file exists
+if [ -f "$CONFIG_FILE" ]; then
+    # If it exists, remove the file
+    rm "$CONFIG_FILE"
+fi
 
 # Check if ports are set
 if [ -z "$ADMIN_PORT" ] || [ -z "$INBOUND_PORT" ]; then
@@ -181,7 +223,6 @@ sudo iptables -A INPUT -p tcp --dport "$INBOUND_PORT" -j ACCEPT
 # Display message
 echo "Ports $ADMIN_PORT and $INBOUND_PORT have been enabled in the firewall."
 
-# Generate the configuration file
 cat <<EOF >${CONFIG_FILE}
 {
   "label": "${ORGANIZATION_ID}_${AGENT_NAME}",
@@ -218,15 +259,21 @@ cat <<EOF >${CONFIG_FILE}
 EOF
 
 FILE_NAME="docker-compose_${ORGANIZATION_ID}_${AGENT_NAME}.yaml"
+
 DOCKER_COMPOSE="${PWD}/${DIR}/${FILE_NAME}"
 
-# Generate the Docker Compose file
+# Check if the file exists
+if [ -f "$DOCKER_COMPOSE" ]; then
+    # If it exists, remove the file
+    rm "$DOCKER_COMPOSE"
+fi
 cat <<EOF >${DOCKER_COMPOSE}
 version: '3'
 
 services:
   agent:
     image: $CREDO_IMAGE
+
     container_name: ${ORGANIZATION_ID}_${AGENT_NAME}
     restart: always
     environment:
@@ -234,13 +281,92 @@ services:
     ports:
      - ${INBOUND_PORT}:${INBOUND_PORT}
      - ${ADMIN_PORT}:${ADMIN_PORT}
-    volumes:
-      - ./agent-config/${ORGANIZATION_ID}_${AGENT_NAME}.json:/config.json
-    command: ["aca-py", "start", "-c", "/config.json"]
+   
+    volumes: 
+      - ./agent-config/${ORGANIZATION_ID}_${AGENT_NAME}.json:/config.json   
+      
+    command: --auto-accept-connections --config /config.json
+      
+volumes:
+  pgdata:
+  agent-indy_client:
+  agent-tmp:
 EOF
 
-END_TIME=$(date +%s)
-EXECUTION_TIME=$((END_TIME - START_TIME))
+# Set ownership and restrict permissions of the .educreds directory
+sudo chown -R $USER: "$DIR"
+chmod -R 700 "$DIR"
 
-echo "Execution time: $EXECUTION_TIME seconds"
-echo "docker-compose path: $DOCKER_COMPOSE"
+if [ $? -eq 0 ]; then
+    cd ${PWD}/${DIR}
+    echo "docker-compose generated successfully!"
+    echo "================="
+    echo "spinning up the container"
+    echo "================="
+    echo "container-name::::::${AGENT_NAME}"
+    echo "file-name::::::$FILE_NAME"
+
+    docker compose -p "${ORGANIZATION_ID}_${AGENT_NAME}" -f $FILE_NAME up -d
+    if [ $? -eq 0 ]; then
+
+        echo "Creating agent config"
+        # Capture the logs from the container
+        container_id=$(docker ps -q --filter "name=${ORGANIZATION_ID}_${AGENT_NAME}")
+
+        if [ -z "$container_id" ]; then
+            echo "Error: No container found with name ${ORGANIZATION_ID}_${AGENT_NAME}"
+            exit 1
+        fi
+
+        # Wait for the container to generate logs
+        retries=5
+        delay=10
+        while [ $retries -gt 0 ]; do
+            container_logs=$(docker logs "$container_id" 2>/dev/null)
+            if [ -n "$container_logs" ]; then
+                break
+            else
+                echo "Waiting for logs to be generated..."
+                sleep $delay
+                retries=$((retries - 1))
+            fi
+        done
+
+        if [ -z "$container_logs" ]; then
+            echo "Error: No logs found for container ${ORGANIZATION_ID}_${AGENT_NAME} after waiting"
+            exit 1
+        fi
+
+        # Extract the token from the logs using sed
+        token=$(echo "$container_logs" | sed -nE 's/.*API Token: ([^ ]+).*/\1/p')
+
+        if [ -z "$token" ]; then
+            echo "Error: Failed to extract API token from logs"
+            exit 1
+        fi
+
+        # Highlight the token line when printing
+        highlighted_token="Token: \x1b[1;31m$token\x1b[0m"
+
+        # Print the extracted token with highlighting
+        echo -e "$highlighted_token"
+        echo "Agent config created"
+
+        # Check if the token exists to determine if the agent is running
+        if [ -n "$token" ]; then
+            echo "Agent is running"
+        else
+            echo "Agent is not running"
+            exit 1
+        fi
+
+    else
+        echo "==============="
+        echo "ERROR : Failed to spin up the agent!"
+        echo "===============" && exit 125
+    fi
+else
+    echo "ERROR : Failed to execute!" && exit 125
+fi
+
+echo "Total time elapsed: $(date -ud "@$(($(date +%s) - $START_TIME))" +%T) (HH:MM:SS)"
